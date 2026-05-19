50e9cb4ab1f66cc10328727af45d794b5caad128
LukasFT <github@lftm.org>
2026-05-19 22:10:56 +0200

    Add TOS accept

--- jobs/setup.sh
+++ jobs/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Accept conda Terms of Service for non-interactive runs
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
 conda create --name semhear python=3.8
 conda activate semhear
 pip install -r requirements.txt
